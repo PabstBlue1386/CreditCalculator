e3a9c3bf9eec4149d0a1f0db5b6a100f185167b2
PabstBlue1386 <longer1386@gmail.com>
2025-05-13 22:55:22 +0500

    Refactoring output info

--- cmd/creditcalculator/main.go
+++ cmd/creditcalculator/main.go
@@ -11,20 +11,20 @@ func main() {
 	var menuNumbers int8
 	var flag bool = false
 
-	fmt.Printf("Добро пожаловать в кредитный калькулятор.\n_________________________________________\n")
+	fmt.Printf("Добро пожаловать в кредитный калькулятор.\n")
 
 	for flag != true {
 
-		menuNumbers = int8(cli.ReadInt("Выберите действие: \n 1. Расчитать платеж по аннуитетному кредиту \n10. Выход из программы"))
+		menuNumbers = int8(cli.ReadInt("\n_________________________________________\n\nВыберите действие: \n 1. Расчитать платеж по аннуитетному кредиту \n10. Выход из программы\n______________________________________\n"))
 		switch menuNumbers {
 		case 1:
 			{
 				fmt.Printf("Введите сумму кредита => ")
 				sumCredit := cli.ReadFloat("Введите сумму кредита => ")
-				term := cli.ReadInt("Введите срок кредита (кол-во месяцев) =>")
+				term := cli.ReadInt("Введите срок кредита (кол-во месяцев) => ")
 				rate := cli.ReadFloat("Введите процентную ставку => ")
 				monthlyPayment := calculator.CalculateAnnuityPayment(int(term), rate, sumCredit)
-				fmt.Printf("Ежемесячный платеж по кредиту будет составлять => %.2f", monthlyPayment)
+				fmt.Printf("\n_________________________________________\nЕжемесячный платеж по кредиту будет составлять => %.2f", monthlyPayment)
 			}
 		case 10:
 			{
