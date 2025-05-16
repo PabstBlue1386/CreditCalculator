827e74a4c7926cbd992fdf57a704d08ab218ab7b
PabstBlue1386 <longer1386@gmail.com>
2025-05-16 22:04:04 +0500

    added func diff payment

--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -15,3 +15,16 @@ func CalculateAnnuityPayment(term int, rate float64, sumCredit float64) float64 
 	return monthlyPayment
 
 }
+
+func CalculateDifferentPayment(term int, rate float64, sumCredit float64) float64 {
+
+	mainDebt := sumCredit / float64(term)
+	currentMonth := 1
+
+	for term > 0 {
+		monthPercentage := (sumCredit - mainDebt*(float64(currentMonth)-1)*(float64(rate/100))) / 12
+
+		term--
+	}
+
+}
